d140abb61ead5a419645867de863346321769204
Astrea_Nugget_Mistress <150022106+Dragnolf17@users.noreply.github.com>
2026-05-08 09:37:18 +0100

    Test3

---
+++ Type Rush/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Type Rush
+//
+//  Created by Aluno a27942 Teste on 08/05/2026.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+}
---
+++ Type RushUITests/Type_RushUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Type_RushUITestsLaunchTests.swift
+//  Type RushUITests
+//
+//  Created by Aluno a27942 Teste on 08/05/2026.
+//
+
+import XCTest
+
+final class Type_RushUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
